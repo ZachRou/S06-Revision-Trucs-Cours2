8969976f2a632f69cfdf40635d05f3dbc4289346
ZachRou <zacrousseau298@gmail.com>
2025-03-11 17:50:47 -0400

    ChampiTry

---
+++ Assets/Scripts/ChampiRandom.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* public class ChampiRandom : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+       transform.position = 
+       float positionX = Random.value(0f, 1f);
+
+        if (Random.value > 0.5f){
+
+                GameObject.Champignons
+
+        } else {
+
+
+        }
+    }   
+}*/
