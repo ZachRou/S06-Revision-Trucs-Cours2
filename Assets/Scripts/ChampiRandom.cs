5c4f6fe7ead9cbea79f44fb841168ecc727d0674
ZachRou <zacrousseau298@gmail.com>
2025-03-13 14:09:07 -0400

    No

--- Assets/Scripts/ChampiRandom.cs
+++ Assets/Scripts/ChampiRandom.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/* public class ChampiRandom : MonoBehaviour
+ public class ChampiRandom : MonoBehaviour
 {
-    // Start is called before the first frame update
+ /* // Start is called before the first frame update
 
     void Start()
     {
@@ -19,5 +19,23 @@ using UnityEngine;
 
 
         }
-    }   
-}*/
+    } */
+
+    [SerializeField] private GameObject Champignons;  
+    [SerializeField] private Transform BGL;           
+    [SerializeField] private Transform BGL3;          
+
+    void Start()
+    {
+        float randomChance = Random.value;
+
+        if (randomChance > 0.5f)
+        {
+            Instantiate(Champignons,BGL.position, Quaternion.identity);
+        }
+        else 
+        {
+            Instantiate(Champignons,BGL3.position, Quaternion.identity); 
+        }
+    }
+}
